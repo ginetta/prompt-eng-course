ad15c8453e77bec57b4a70ff9d1644ce05c41c9d
Nelson Fonseca <nelsonfonseca.18@gmail.com>
2024-05-03 13:03:31 +0100

    feat: pass params to submitUserMessage (#5)

--- components/ui/sliderWithLabel.tsx
+++ components/ui/sliderWithLabel.tsx
@@ -11,23 +11,13 @@ export const SliderWithLabel: FC<SliderWithLabelProps> = ({
   label,
   ...props
 }) => {
-  const [value, setValue] = useState(50);
-
   return (
     <div className="flex h-full w-[300px] flex-col items-start justify-center space-y-4 bg-muted/50 dark:bg-background">
       <div className="flex w-full justify-between">
         <h3 className="text-lg text-gray-400">{label}</h3>
-        <h4>{value}</h4>
+        <h4>{props.value}</h4>
       </div>
-      <Slider
-        defaultValue={[value]}
-        max={100}
-        step={1}
-        onValueChange={(e) => {
-          setValue(e[0]);
-        }}
-        {...props}
-      />
+      <Slider {...props} />
     </div>
   );
 };
